866dd8b95d2f2c2e685e291a0c3bf6bb87d065bf
Quantum179 <steeven.tell@gmail.com>
2020-05-23 11:27:23 -0400

    change port

--- web/src/App.vue
+++ web/src/App.vue
@@ -115,7 +115,7 @@ export default {
     }
 
     const connection = process.env.NODE_ENV === 'production'
-      ? 'https://ouboude-api.herokuapp.com:36762'
+      ? 'https://ouboude-api.herokuapp.com:37990'
       : 'http://localhost:5000'
 
     this.socket = io.connect(connection, { transports: ['websocket'], upgrade: false })
